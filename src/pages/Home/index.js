eaf07057c6a5045f47cef30d3290e12d6f9af7cc
Chad Cook <charliert13@yahoo.com>
2021-08-08 19:00:19 -0500

    created a navigation component and added it to the home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "../../components/navbar";
+import "./style.css";
+
+const Home = () => {
+
+    return(
+        <div id="landingContainer">
+            <Navigation />
+        </div>
+    )
+}
+
+export default Home;
