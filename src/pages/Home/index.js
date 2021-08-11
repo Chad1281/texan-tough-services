6f200d667aef3eb821392dbeb19508d09d5ff43d
Chad Cook <charliert13@yahoo.com>
2021-08-10 21:04:46 -0500

    replaced some of the images and used Texas flag color scheme for the components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,7 +6,9 @@ import Parallax from "../../components/parallax";
 import Section from "../../components/section";
 import Barn from "../../assets/images/barn.png";
 import BuiltIn from "../../assets/images/builtin2.jpg";
-import Porch from "../../assets/images/trellis.jpg";
+import Trellis from "../../assets/images/trellis.jpg";
+import Home1 from "../../assets/images/home1.jpg";
+import Kitchen from "../../assets/images/kitchen3.jpg"
 import "./style.css";
 import Card from "../../components/card";
 
@@ -15,19 +17,22 @@ const Home = () => {
     return(
         <div id="homeContainer">
             <Navigation />
-            <Header bkgdImg={ Barn } >
-                <Card cardClass="card" h2="Heading" p1="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio justo. Proin aliquam iaculis arcu. Praesent egestas arcu nunc, ut condimentum tellus iaculis non. Suspendisse lobortis sodales pharetra. Suspendisse imperdiet libero eu neque lacinia sollicitudin. Vivamus lacus mauris, aliquam eu augue nec, rutrum suscipit tellus. Sed eget lectus imperdiet, posuere turpis eget, convallis purus. Mauris a nibh at libero condimentum scelerisque quis ac turpis. Donec vulputate ultrices venenatis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam pulvinar ullamcorper elementum." />
+            <Header bkgdImg={ Trellis } >
+                <Card cardClass="headerCard" h2="TRUSTED HOME CONSTRUCTION SERVICE" 
+                p1="Family and Locally Owned"
+                p2="Providing New and Renovation Construction"
+                p3="Serving Chambers County and Surrounding Areas" />
             </Header>
             <Section>
                 <Card cardClass="card" h2="Heading" p1="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio justo. Proin aliquam iaculis arcu. Praesent egestas arcu nunc, ut condimentum tellus iaculis non. Suspendisse lobortis sodales pharetra. Suspendisse imperdiet libero eu neque lacinia sollicitudin. Vivamus lacus mauris, aliquam eu augue nec, rutrum suscipit tellus. Sed eget lectus imperdiet, posuere turpis eget, convallis purus. Mauris a nibh at libero condimentum scelerisque quis ac turpis. Donec vulputate ultrices venenatis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam pulvinar ullamcorper elementum." />
             </Section>
-            <Parallax custClass="builtin" bkgdImg={BuiltIn}>
+            <Parallax custClass="builtin" bkgdImg={Home1}>
                 <Card cardClass="card"  h2="Heading" p1="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio justo. Proin aliquam iaculis arcu. Praesent egestas arcu nunc, ut condimentum tellus iaculis non. Suspendisse lobortis sodales pharetra. Suspendisse imperdiet libero eu neque lacinia sollicitudin. Vivamus lacus mauris, aliquam eu augue nec, rutrum suscipit tellus. Sed eget lectus imperdiet, posuere turpis eget, convallis purus. Mauris a nibh at libero condimentum scelerisque quis ac turpis. Donec vulputate ultrices venenatis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam pulvinar ullamcorper elementum." />
             </Parallax>
             <Section>
-                <Card cardClass="card"  h2="Heading" p1="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio justo. Proin aliquam iaculis arcu. Praesent egestas arcu nunc, ut condimentum tellus iaculis non. Suspendisse lobortis sodales pharetra. Suspendisse imperdiet libero eu neque lacinia sollicitudin. Vivamus lacus mauris, aliquam eu augue nec, rutrum suscipit tellus. Sed eget lectus imperdiet, posuere turpis eget, convallis purus. Mauris a nibh at libero condimentum scelerisque quis ac turpis. Donec vulputate ultrices venenatis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam pulvinar ullamcorper elementum." />
+                <Card cardClass="card"  h2="Heading" />
             </Section>
-            <Parallax custClass="porch" bkgdImg={Porch}>
+            <Parallax custClass="porch" bkgdImg={BuiltIn}>
                 <Card cardClass="card"  h2="Heading" p1="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at odio justo. Proin aliquam iaculis arcu. Praesent egestas arcu nunc, ut condimentum tellus iaculis non. Suspendisse lobortis sodales pharetra. Suspendisse imperdiet libero eu neque lacinia sollicitudin. Vivamus lacus mauris, aliquam eu augue nec, rutrum suscipit tellus. Sed eget lectus imperdiet, posuere turpis eget, convallis purus. Mauris a nibh at libero condimentum scelerisque quis ac turpis. Donec vulputate ultrices venenatis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam pulvinar ullamcorper elementum." />
             </Parallax>
             <Footer />
